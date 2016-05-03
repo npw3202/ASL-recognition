4ffc1d1ab47b27d90d9318a6e3f8973e6b8ec2a4
Nicholas Wilkins <npw3202@rit.edu>
2016-05-02 21:32:19 -0400

    Added Locator for real though

---
+++ src/ComputerVision/Locator.java
@@ -0,0 +1,20 @@
+package ComputerVision;
+
+public class Locator {
+	/**
+	 * Determines the 2d location of the palm
+	 * @return a float array containing the 2d location of the palm
+	 */
+	public float[] getPalmLoc(){
+		float[] toReturn = new float[3];
+		return toReturn;
+	}
+	/**
+	 * Determines the 2d location of the head
+	 * @return a float array containing the 2d location of the head
+	 */
+	public float[] getHeadLoc(){
+		float[] toReturn = new float[3];
+		return toReturn;
+	}
+}
